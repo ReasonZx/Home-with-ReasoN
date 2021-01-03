fc6e45bde0bee5bcfaaed1676665c75ad4cea83b
Xico <xico.reasonzx@gmail.com>
2021-01-03 22:22:32 +0000

    Main commit (3)

---
+++ functions/src/config.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var config = {
+    apiKey: "AIzaSyCkGpIs7lWUg7bTvUPO2s8S3rOgnOxSE5A",
+    authDomain: "lasthome-869f7.firebaseapp.com",
+    databaseURL: "https://lasthome-869f7.firebaseio.com",
+    projectId: "lasthome-869f7",
+    storageBucket: "lasthome-869f7.appspot.com",
+    messagingSenderId: "569340367330"
+  };
+  firebase.initializeApp(config);
+  
+  var CLIENT_ID =
+      'null';
